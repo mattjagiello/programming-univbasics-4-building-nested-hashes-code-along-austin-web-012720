6c92e5875dd96e2f811a31ff8743e5236fdd6138
Matthew Jagiello <mjagiello@gmail.com>
2020-01-18 09:51:42 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,9 +5,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
+        {
         :name => "Benvolio",
         :age => "17",
         :attitude => "worried"
+        }
+        {
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
+        }
       ]
    },
    :capulet => {
@@ -15,9 +22,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        :name => "Mercutio",
-        :age => "18",
-        :attitude => "hot-headed"
+
       ]
    }
 }
